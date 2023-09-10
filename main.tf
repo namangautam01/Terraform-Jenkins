49e390a859520ad3ed32c9b4027b8df724e6a27e
namangautam01 <56624502+namangautam01@users.noreply.github.com>
2023-09-10 19:27:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resources "aws_instance" "foo" {
+  count = 4 
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
